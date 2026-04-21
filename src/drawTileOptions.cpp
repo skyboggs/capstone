d4d9b71650f99ae1ea8368ec1080b64b093b66ca
skyboggs <55774849+skyboggs@users.noreply.github.com>
2026-04-21 14:52:23 -0700

    chore: fixed the structure of the program and added documentation

--- src/drawTileOptions.cpp
+++ src/drawTileOptions.cpp
@@ -10,20 +10,74 @@ using namespace std;
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 600
 
-void drawTileCompadabilities
+
+
+struct textureMapping
+{
+  Image image;
+  Texture2D texture;
+
+  vector<Rectangle> sourceRecs;
+  vector<Rectangle> destRecs;
+
+  bool debugMode = false;
+};
+
+struct visualizerSettings
+{
+  bool drawLines   = true;
+  bool drawWithGap = true;
+  bool highlight   = true;
+  int gapSize      = 10;
+
+  Color highlightColor = GREEN;
+};
+
+void updateConfig(visualizerSettings& visConfig)
+{
+  // toggling drawing the lines for the grid
+  if(IsKeyPressed(KEY_L)) { visConfig.drawLines = !visConfig.drawLines; }
+  
+  // toggling adding a visual gap between tiles
+  if(IsKeyPressed(KEY_G)) { visConfig.drawWithGap = !visConfig.drawWithGap; }
+  
+  // toggling adding a highlight around valid cell options
+  if(IsKeyPressed(KEY_H)) { visConfig.highlight = !visConfig.highlight; }
+}
+
+void printVec(const Vector2& v, const string vName)
+{
+  cout << endl;
+  cout << "-- -- -- " << vName << "-- -- -- " << endl;
+  cout << "  " << vName << ".x : " << v.x << endl;
+  cout << "  " << vName << ".y : " << v.y << endl;
+  cout << endl;
+}
+
+void drawTileCompatabilities
 (
-  bool drawLines,
-  bool drawWithGap,
-  vector<Rectangle>& tileSourceRecs,
-  vector<Rectangle>& tileDestRecs,
-  Texture2D currentTexture,
-  Vector2 tileDims
+  visualizerSettings visConfig,
+  textureMapping detailMapping,
+  Vector2 tileDims,
+  vector<bool> validOptions
 )
 {
-	Vector2 tileWindowDims{currentTexture.width - tileDims.x, currentTexture.height - tileDims.y};
+  vector<Rectangle>& tileSourceRecs = detailMapping.sourceRecs;
+  vector<Rectangle>& tileDestRecs   = detailMapping.destRecs;
+  Texture2D currentTexture          = detailMapping.texture;
 
-  int gapSize = 10;
+// extracting our settings
+  bool&  drawLines      = visConfig.drawLines;
+  bool&  drawWithGap    = visConfig.drawWithGap;
+  int&   gapSize        = visConfig.gapSize;
+  bool&  highlight      = visConfig.highlight;
+  Color& highlightColor = visConfig.highlightColor;
+
+// setting tileWindowDims
+  // holds the dimensions of how many tiles fit in the x and y direction on the screen
+	Vector2 tileWindowDims{currentTexture.width - tileDims.x, currentTexture.height - tileDims.y};
   
+// drawing all of the destination rectangles
   for(int i=0;i<tileSourceRecs.size();++i)
   {
   	Rectangle currentRectangle = tileDestRecs[i];
@@ -36,63 +90,56 @@ void drawTileCompadabilities
 
   		currentRectangle.width  -= gapSize;
   		currentRectangle.height -= gapSize;
+
+      DrawRectangleRec(tileDestRecs[i],BLACK);
   	}
-    /*
-    if(i < 10)
-    {
-      cout << endl;
-      cout << "rectangles drawing[" << i << "]:" << endl;
-      cout << " source:" << endl;
-      cout << tileSourceRecs[i].x << " " << tileSourceRecs[i].y << " " << tileSourceRecs[i].width << " " << tileSourceRecs[i].height << endl;
-      cout << " dest:" << endl;
-      cout << currentRectangle.x << " " << currentRectangle.y << " " << currentRectangle.width << " " << currentRectangle.height << endl;
-      cout << endl;
-    }
-    */
   
-  	//DrawTexturePro(Texture2D texture, Rectangle source, Rectangle dest, Vector2 origin, float rotation, Color tint);
-  	DrawTexturePro(currentTexture,tileSourceRecs[i],currentRectangle,Vector2{0.0f,0.0f},0.0f,WHITE);
-
-    const Rectangle& currentRec = tileDestRecs[i];
-    Color currentColor = WHITE;
-
-    switch(i % 4)
-    {
-      case 0:
-        currentColor = Color{ 230, 41, 55, 100};
-        break;
-      case 1:
-        currentColor = Color{ 0, 121, 241, 100};
-        break;
-      case 2:
-        currentColor = Color{255, 161, 0, 100};
-        break;
-      case 3:
-        currentColor = Color{0, 228, 48, 100};
-        break;
-    }
-
-    DrawRectangleRec(currentRec,currentColor);
-
-  	//DrawTexturePro(testTexture,tileSourceRecs[i],tileDestRecs[i],Vector2{0.0f,0.0f},0.0f,(i % 2 == 0 ? BLUE : RED));
+  	DrawTexturePro(currentTexture,tileSourceRecs[i],currentRectangle,Vector2{0.0f,0.0f},0.0f,(highlight && validOptions[i]) ? highlightColor : Color{150,150,150,255});
   }
   
-  
-  // drawing lines to add some seperation between the tiles
+// drawing lines to add some seperation between the tiles
   if(drawLines)
   {
   	Color lineColor = (drawWithGap ? WHITE : BLACK);
   
-  	// drawing a vertical line to show where the tiles are
-  	for(int i=1;i<=tileWindowDims.y;++i)
+  // drawing a vertical line to show where the tiles are
+  	for(int i=1;i<=tileWindowDims.x;++i)
   	{
-  		DrawLine(tileDestRecs[0].x,tileDestRecs[0].y + (i * tileDestRecs[0].height),tileDestRecs[0].x + ((tileWindowDims.x + 1) * tileDestRecs[0].width),tileDestRecs[0].y + (i * tileDestRecs[0].height),lineColor);
+  		DrawLineEx
+      (
+        Vector2
+        {
+          tileDestRecs[0].x + (i * tileDestRecs[0].width),
+          tileDestRecs[0].y
+        },
+        { 
+          tileDestRecs[0].x + (i * tileDestRecs[0].width),
+          tileDestRecs[0].y + ((tileWindowDims.y + 1) * tileDestRecs[0].height),
+        },
+        1.0f,
+        lineColor
+      );
   	}
   
-  	// drawing a horizontal line to show where the tiles are
-  	for(int m=1;m<=tileWindowDims.x;++m)
+  // drawing a horizontal line to show where the tiles are
+  	for(int m=1;m<=tileWindowDims.y;++m)
   	{
-  		DrawLine(tileDestRecs[0].x + ((float)m * tileDestRecs[0].width),tileDestRecs[0].y,tileDestRecs[0].x + ((float)m * tileDestRecs[0].width),tileDestRecs[0].y + ((tileWindowDims.y + 1) * tileDestRecs[0].height),lineColor);
+      DrawLineEx
+      (
+        Vector2
+        {
+          tileDestRecs[0].x,
+          tileDestRecs[0].y + (m * tileDestRecs[0].height)
+        },
+        Vector2
+        {
+          tileDestRecs[0].x + ((float)(tileWindowDims.x + (float)1) * tileDestRecs[0].width),
+          tileDestRecs[0].y + (m * tileDestRecs[0].height)
+        },
+        1.5f,
+        lineColor
+      );
+
   	}
   }
 }
@@ -103,19 +150,27 @@ void drawTileCompadabilities
 
 void generateTileRecs
 (
- vector<Rectangle>& sourceRecs,  // the source locations of rectangles on the texture
- vector<Rectangle>& destRecs,    // the destination location of rectangles on the screen
- const Texture2D& sourceTexture, // the texture that we are using <-- just need the dimensions from the texture
- const Vector2& tileDims,        // the size of the tiles we are working with
- bool printDebugInfo
+ textureMapping& detailMapper,
+ const Vector2& tileDims // the size of the tiles we are working with
 )
 {
+// extracting our variables
+ vector<Rectangle>& sourceRecs    = detailMapper.sourceRecs; // the source locations of rectangles on the texture
+ vector<Rectangle>& destRecs      = detailMapper.destRecs;   // the destination location of rectangles on the screen
+ const Texture2D&   sourceTexture = detailMapper.texture;    // the texture that we are using <-- just need the dimensions from the texture
+                                                          
+// setting genTileList options                                 
+  bool mirroredTiles      (false);
+  bool printGeneratedTiles(false);
+  bool testing            (false);
+
+// calculating variables for tiles
 	// resolution of the texture that was inputted
 	Vector2 imageRes{(float)sourceTexture.width,(float)sourceTexture.height}; 
 
+
 	// holds a vector of the tiles that go over the inputted texture
-	//vector<tile> tileHolder = genTileList(imageRes.x,imageRes.y,tileDims.x,tileDims.y,false,true,false);
-	vector<tile> tileHolder = genTileList(imageRes.x,imageRes.y,tileDims.x,tileDims.y,false,false,false);
+	vector<tile> tileHolder = genTileList(imageRes.x,imageRes.y,tileDims.x,tileDims.y,mirroredTiles,printGeneratedTiles,testing);
 
 	// holds the dimensions of how many tiles fit in the x and y direction on the screen
 	Vector2 tileWindowDims{(imageRes.x-tileDims.x) + 1, (imageRes.y-tileDims.y) + 1};
@@ -123,28 +178,21 @@ void generateTileRecs
 	// holds the dimensions of the tile windows that are drawn to screen
 	Vector2 tileDisplayDims{static_cast<float>(WINDOW_WIDTH)/tileWindowDims.x,static_cast<float>(WINDOW_HEIGHT)/tileWindowDims.y};
 
-
-  cout << endl;
-  cout << endl;
-  cout << "tileDisplayDims:" << endl;
-  cout << "  " << tileDisplayDims.x << " x " << tileDisplayDims.y << endl;
-  cout << endl;
-  cout << endl;
-
-
-
+// generating source rectangles
 	// generating and putting all the source recs in a list
 	for(int i=0;i<tileHolder.size();++i)
 	{
 		sourceRecs.push_back(tileHolder[i].generateRec());
 	}
 
-
+// generating variables for destination rectangles
 	// how much the individual tiles need to be scaled up to fit in their displays
 	Vector2 tileScale{tileDisplayDims.x/tileDims.x,tileDisplayDims.y/tileDims.y};
 
 	if(tileScale.x < tileScale.y) { tileScale.y = tileScale.x; }
 	if(tileScale.y < tileScale.x) { tileScale.x = tileScale.y; }
+
+  Vector2 displayDimensions{tileScale.x * tileDims.x,tileScale.y * tileDims.y};
 
 
 	Vector2 scaledTileSize
@@ -153,26 +201,20 @@ void generateTileRecs
     (float)static_cast<int>(tileDims.y * tileScale.y)
   };
 
-
-	if(printDebugInfo)
+// printing debug info
+	if(detailMapper.debugMode)
 	{
-		cout << endl;
-		cout << "-- -- -- tileWindowDims -- -- --" << endl;
-		cout << "tileWindowDims.x : " << tileWindowDims.x << endl;
-		cout << "tileWindowDims.y : " << tileWindowDims.y << endl;
-		cout << "-- -- -- -- -- -- -- -- -- -- -- " << endl;
-		cout << endl;
-		cout << "tileScale.x : " << tileScale.x << endl;
-		cout << "tileScale.y : " << tileScale.y << endl;
+    printVec(tileDisplayDims,"tileDisplayDims");
+    printVec(displayDimensions,"displayDimensions");
+    printVec(tileWindowDims,"tileWindowDims");
+    printVec(tileScale,"tileScale");
 	}
 
+// generating destination rectangles
 	for(int i=0;i<tileWindowDims.y;++i)
 	{
 		for(int m=0;m<tileWindowDims.x;++m)
 		{
-			//destRecs.push_back(Rectangle{imageRes.x * i,imageRes.y,tileDims.x,tileDims.y});
-			//destRecs.push_back(Rectangle{tileDisplayDims.x * m,tileDisplayDims.y * i,tileDims.x,tileDims.y});
-			//destRecs.push_back(Rectangle{tileDisplayDims.x * m,tileDisplayDims.y * i,tileDims.x * tileScale.x,tileDims.y * tileScale.y});
 			destRecs.push_back
       (
         Rectangle
@@ -183,45 +225,9 @@ void generateTileRecs
           scaledTileSize.y
         }
       );
-			if(printDebugInfo)
-			{
-        if(i < 4 && m < 4)
-        {
-          cout << endl;
-          cout << "pushing back dest Rec:" << endl;
-          cout << "  tileDisplayDims.x * m: " << (tileDisplayDims.x * m) << endl;
-          cout << "  tileDisplayDims.y * i: " << (tileDisplayDims.y * i) << endl;
-          cout << "  tileDims.x           : " << tileDims.x << endl;
-          cout << "  tileDims.y           : " << tileDims.y << endl;
-          cout << "  rectangle: { ";
-          cout << destRecs.at(destRecs.size()-1).x << ", ";
-          cout << destRecs.at(destRecs.size()-1).y << ", ";
-          cout << destRecs.at(destRecs.size()-1).width << ", ";
-          cout << destRecs.at(destRecs.size()-1).height << "}" << endl;
-          cout << endl;
-        }
-			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-// things needed to make this a function call
-//   window dimensions to have accurate drawing proportions
-//   current texture we are drawing
-//   tile dimensions
-
-
-
 
 
 
@@ -238,12 +244,14 @@ int main(int argc, const char** argv)
 	int tileWidth  = 3;
 	int tileHeight = 3;
 
+// pushing inputting arguments to the command stack
 	for(int i=1;i<argc;++i)
 	{
 		commandStack.push_back(argv[i]);
-	//cout << "pushed: " << commandStack[commandStack.size()-1] << endl;
+	  cout << "pushed: " << commandStack[commandStack.size()-1] << endl;
 	}
 
+// processing the command stack and setting program configuration
 	for(int i=0;i<commandStack.size();++i)
 	{
 		int remainingArgs = (commandStack.size() - i - 1);
@@ -295,122 +303,125 @@ int main(int argc, const char** argv)
 		}
 	}
 	
-
-
-
-
-
-
-
-  /////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////
-  ////////////////// drawing section //////////////////
-  /////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////
-  /////////////////////////////////////////////////////
-
-
-
-
-
 	InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"visualizing tiles");
 
+// declaring our variables
+  float currentAngle(0.0f);
+  textureMapping generationDetails;
+  visualizerSettings visConfig;
 
-	Image testImage = LoadImage(imagePath.c_str());
-	Texture2D testTexture = LoadTextureFromImage(testImage);
+/////////////////////////////////////////////////////
+////// setting our variables and parameters /////////
+/////////////////////////////////////////////////////
+
+// selecting how big a tile is in our case
+	Vector2 tileDims{(float)tileWidth,(float)tileHeight};
 	//Vector2 tileDims{(float)testTexture.width-2,(float)testTexture.height-2};
 	//Vector2 tileDims{(float)7,(float)7};
-	Vector2 tileDims{(float)tileWidth,(float)tileHeight};
 	//Vector2 tileDims{(float)1,(float)1};
 
-	cout << "Tile size: " << tileDims.x << " x " << tileDims.y << endl;
-	vector<Rectangle> tileSourceRecs;
-	vector<Rectangle> tileDestRecs;
+	//cout << "Tile size: " << tileDims.x << " x " << tileDims.y << endl;
 
-	// generating source and destination recs
-	generateTileRecs(tileSourceRecs,tileDestRecs,testTexture,tileDims,true);
+// setting generationDetails variables
+  generationDetails.image     = LoadImage(imagePath.c_str());
+  generationDetails.texture   = LoadTextureFromImage(generationDetails.image);
+  generationDetails.debugMode = false;
 
-	bool drawLines = true;
-	bool drawWithGap = true;
+// setting generationDetails Rectangles
+  generateTileRecs(generationDetails,tileDims);
+  
+  vector<Rectangle>& tileSourceRecs = generationDetails.sourceRecs;
+  vector<Rectangle>& tileDestRecs   = generationDetails.destRecs;
 
-	// holds the dimensions of how many tiles fit in the x and y direction on the screen
-	//Vector2 tileWindowDims{testTexture.width - tileDims.x, testTexture.height - tileDims.y};
+// getting the dimensions of the window that the compatability visualizer is being drawn in
+  const Rectangle& lastRec = tileDestRecs.at(tileDestRecs.size()-1);
 
-  //RenderTexture2D t1 = LoadRenderTexture(testTexture.width, testTexture.height);
-  RenderTexture2D t1 = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
+  Vector2 displayDimensions
+  {
+    (lastRec.x + lastRec.width)  - tileDestRecs[0].x,
+    (lastRec.y + lastRec.height) - tileDestRecs[0].y
+  };
 
-  BeginTextureMode(t1);
-    //ClearBackground(WHITE);
-  EndTextureMode();
-
-  RenderTexture2D background = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
+// initializiing our textures and drawing the background
+  RenderTexture2D t1         = LoadRenderTexture(displayDimensions.x, displayDimensions.y);
+  RenderTexture2D background = LoadRenderTexture(WINDOW_WIDTH       , WINDOW_HEIGHT      );
 
   BeginTextureMode(background);
     ClearBackground(BLACK);
 		drawCheckeredBackground(WINDOW_WIDTH,WINDOW_HEIGHT, 40,Color{0,170,100,100}, Color{0,200,100,100});
   EndTextureMode();
-  
 
+// marking a compatability list to change which tiles should be marked as compatible
+  vector<bool> compatibleList(tileDestRecs.size(),false);
+
+  try
+  {
+    for(int i=0;i<400;i+=(9 + ((i * i) % 7)))
+    {
+      compatibleList.at(i) = true;
+    }
+    compatibleList.at(4  ) = true;
+    compatibleList.at(9  ) = true;
+    compatibleList.at(39 ) = true;
+    compatibleList.at(49 ) = true;
+    compatibleList.at(149) = true;
+    compatibleList.at(119) = true;
+    compatibleList.at(129) = true;
+    compatibleList.at(139) = true;
+  }catch(out_of_range e)
+  {
+    cout << "out of range" << endl;
+  }
+
+
+//////////////////////////
+//// draw loop ///////////
+//////////////////////////
 	while(!WindowShouldClose())
 	{
-		// toggling drawing the lines for the grid
-		if(IsKeyPressed(KEY_L)) { drawLines = !drawLines; }
-
-		// toggling adding a visual gap between tiles
-		if(IsKeyPressed(KEY_G)) { drawWithGap = !drawWithGap; }
+  // checking for new user input
+    updateConfig(visConfig);
 
 
-    // updating the compatability texture
+// updating the compatability texture
     BeginTextureMode(t1);
       ClearBackground(BLANK);
-      //DrawRectangle(0,0,100,100,RED);
-      drawTileCompadabilities(drawLines,drawWithGap,tileSourceRecs,tileDestRecs,testTexture,tileDims);
+      drawTileCompatabilities(visConfig,generationDetails,tileDims,compatibleList);
     EndTextureMode();
 
-    float x,y,w,h;
-    x = 0.0f;
-    y = 0.0f;
-    w = static_cast<float>(WINDOW_WIDTH);
-    h = static_cast<float>(WINDOW_HEIGHT);
-
-
-
+  // starting to draw
 		BeginDrawing();
 		  ClearBackground(BLACK);
 
-      // drawing background
+  // drawing background
       DrawTexturePro
       (
         background.texture,
         (Rectangle){ 0, 0, (float)WINDOW_WIDTH, -(float)WINDOW_HEIGHT },
-        (Rectangle){ x, y, w, h},
+        (Rectangle){ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT},
         Vector2{0,0},
         0.0f,
         WHITE
       );
 
-      // drawing compatability visualizer
+  // drawing compatability visualizer
       DrawTexturePro
       (
         t1.texture,
-        //(Rectangle){ 0, 0, (float)testTexture.width, -(float)testTexture.height },
-        (Rectangle){ 0, 0, (float)WINDOW_WIDTH, -(float)WINDOW_HEIGHT},
-        (Rectangle){ x, y, w, h},
-        Vector2{0,0},
-        0.0f,
+        (Rectangle){ 0, 0, (float)displayDimensions.x, -(float)displayDimensions.y},
+        (Rectangle){ (float)WINDOW_WIDTH/2.0f, (float)WINDOW_HEIGHT/2.0f, 400, 400},
+        Vector2{(float)200.0f,(float)200.0f},
+        currentAngle,
         WHITE
       );
 
-
-
-      //drawTileCompadabilities(drawLines,drawWithGap,tileSourceRecs,tileDestRecs,testTexture,tileDims);
+    //currentAngle += 0.2f;
 
 		EndDrawing();
 	}
 
-	UnloadImage(testImage);
-	UnloadTexture(testTexture);
+	UnloadImage(generationDetails.image);
+	UnloadTexture(generationDetails.texture);
 
 	CloseWindow();
 
